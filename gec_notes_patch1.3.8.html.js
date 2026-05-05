e6c70a08b32cafbef66dfe58a8f948ebce069d5f
Michael Schäfer <126463259+mssoftag@users.noreply.github.com>
2026-05-05 15:51:07 +0200

    Add files via upload

---
+++ gec_notes_patch1.3.8.html.js
@@ -0,0 +1,25 @@
+const releaseNotes = {
+  pageTitle: "GEC Notes Template",
+
+  badge: "Release Notes",
+
+  titlePrefix: "What's New in",
+  patchVersion: "Patch 1.3.8",
+  subtitle: "Neue Funktionen, Verbesserungen und relevante Änderungen auf einen Blick.",
+
+  startDate: "04/01/2026",
+  releaseDate: "04/22/2026",
+
+  featureTitle: "New Features",
+  sectionKicker: "Patch Highlights",
+
+  features: [
+    "Feature 1 Laasdfasdfasdfasdfas asdfasdfasdfasd asdfasdfasdf asdfasdfasdf asdfasdfsa asdfasdf",
+    "Feature 2",
+    "Feature 3",
+    "Feature 4"
+  ],
+
+  footerOwner: "GEC Product Management",
+  generatedOn: "05/05/2026 12:46 pm"
+};
